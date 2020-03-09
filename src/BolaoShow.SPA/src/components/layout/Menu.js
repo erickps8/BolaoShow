9c71107ded5dd4f068294f11a688ab955618c4e4
erickps8 <erickps8@hotmail.com>
2020-03-09 17:41:52 -0300

    custom_menu

--- src/BolaoShow.SPA/src/components/layout/Menu.js
+++ src/BolaoShow.SPA/src/components/layout/Menu.js
@@ -7,17 +7,18 @@ import NavbarAuthenticated from './NavbarAuthenticated';
 const Menu = () => {
     return (
             <nav className="navbar navbar-default Menu-principal">
-                <div className="container-fluid">
+                <div className="container">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                             <span className="sr-only">Toggle navigation</span>
                         </button>
 
-                        <a className="navbar-brand" href="/home"><p className="col-md-2">BOLÃO SHOW</p></a>
+                        <a className="navbar-brand" href="/home"><p>BOLÃO SHOW</p></a>
                     </div>
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav">
-                            <NavbarAuthenticated/>
+                        <li><Link to="/home">Home</Link></li>
+                            <NavbarAuthenticated/>                            
                             <li><Link to="/sobre">Sobre</Link></li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">                            
